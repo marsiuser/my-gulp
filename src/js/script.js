e3ad0c7e4a9885eac9ffc5155e393d5f79774444
Arina <45823655+marsiuser@users.noreply.github.com>
2020-09-02 16:35:02 +0300

    luxtrader 2 sections

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,3 @@
-@@include('alert.js')
 
 function testWebP(callback) {
 
@@ -17,3 +16,45 @@ function testWebP(callback) {
     document.querySelector('body').classList.add('no-webp');
     }
 });
+
+let userHeaderIcon = document.querySelector(".user-header__icon");
+
+userHeaderIcon.addEventListener("click", function(e){
+    let userHeaderMenu = document.querySelector(".user-header__menu");
+    userHeaderMenu.classList.toggle('_active');
+    iconMenu.classList.remove('_active');
+    menuBodyOpen.classList.remove('_active');
+});
+
+let iconMenu = document.querySelector(".icon-menu");
+let menuBodyOpen = document.querySelector(".menu__body");
+
+iconMenu.addEventListener("click", function(e){
+    iconMenu.classList.toggle('_active');
+    menuBodyOpen.classList.toggle('_active');
+});
+
+document.addEventListener("click", function(e){
+    if(!e.target.closest('.user-header')){
+        let userHeaderMenu = document.querySelector(".user-header__menu");
+        userHeaderMenu.classList.remove('_active');
+    }
+});
+
+$(document).ready(function(){
+    $('.slider').slick({
+        dots:false,
+        prevArrow:"<img class='a-left control-c prev slick-prev' src='../img/prev.jpg'>",
+        nextArrow:"<img class='a-right control-c next slick-next' src='../img/next.jpg'>"
+    });
+    $(".control-c").wrapAll("<div class='new-nav'></div>");
+    $('.product-slider').slick({
+        dots:false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        prevArrow:"<img class='a-left control-c prev slick-prev' src='../img/left-arrow.png'>",
+        nextArrow:"<img class='a-right control-c next slick-next' src='../img/right-arrow.png'>"
+    });
+});
+
